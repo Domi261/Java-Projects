4af93f80adfe7896a9924534664c4837e6047ffd
Domi261 <dominick.schustereit@outlook.de>
2024-06-27 16:02:09 +0200

    Erste Schritte

---
+++ Project 1/CryptoConverter.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CryptoConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Geben Sie den Betrag in Bitcoins ein: ");
+        double bitcoin = scanner.nextDouble();
+
+        System.out.println("Geben Sie den Wechselkurs (Bitcoins zu Franken) ein:");
+        double exchangeRate = scanner.nextDouble();
+
+        double francs = bitcoin * exchangeRate;
+
+        System.out.printf("%.2f Bitcoins entsprechen %.2f Franken%n", bitcoin, francs);
+
+        scanner.close();
+    }
+}
