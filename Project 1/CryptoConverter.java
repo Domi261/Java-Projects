8a798de1a628ff8d1bba28c89d7e0544284d1f71
Domi261 <dominick.schustereit@outlook.de>
2024-07-01 07:19:49 +0200

    user validation

--- Project 1/CryptoConverter.java
+++ Project 1/CryptoConverter.java
@@ -3,23 +3,46 @@ import java.util.Scanner;
 public class CryptoConverter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int currency = 0;
 
-        System.out.println("Herzlich willkommen beim Kryptomenü"); 
+        System.out.println("* Herzlich willkommen beim Kryptomenü *"); 
         System.out.println("-------------------------------------");
 
-        System.out.println("Geben Sie den Betrag in Bitcoins ein: ");
-        double bitcoin = scanner.nextDouble();
+        while (currency < 1 || currency > 3) {
+            System.out.println("Bitte wählen Sie ihre gewünschte Kryptowährung aus: ");
+            System.out.println("Bitcoin:\t1");
+            System.out.println("Ethereum:\t2");
+            System.out.println("Solana:\t\t3");
 
-        System.out.println("Geben Sie den Wechselkurs (Bitcoins zu Franken) ein:");
-        double exchangeRate = scanner.nextDouble();
+            if (scanner.hasNextInt()) {
+                currency = scanner.nextInt();
 
-        double francs = bitcoin * exchangeRate;
+                if (currency < 1 || currency > 3) {
+                    System.out.println("Ungültige Auswahl. Bitte wählen Sie eine Zahl zwischen 1 und 3.");
+                }
+            } else {
+                System.out.println("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.");
+                scanner.next(); 
+            }
+        }
 
-        System.out.printf("%.2f Bitcoins entsprechen %.2f Franken%n", bitcoin, francs);
-
+        
         scanner.close();
+
+
+        switch(currency) {
+            case 1:
+                System.out.println("Sie haben Bitcoin gewählt.");
+                System.out.println("Bitcoin (BTC) ist eine dezentrale digitale Währung.");
+                break;
+            case 2:
+                System.out.println("Sie haben Ethereum gewählt.");
+                System.out.println("Ethereum (ETH) ist eine dezentrale Plattform, die Smart Contracts ermöglicht.");
+                break;
+            case 3:
+                System.out.println("Sie haben Solana gewählt.");
+                System.out.println("Solana (SOL) ist eine hochleistungsfähige Blockchain-Plattform.");
+                break;
+        }
     }
 }
-
-
-
